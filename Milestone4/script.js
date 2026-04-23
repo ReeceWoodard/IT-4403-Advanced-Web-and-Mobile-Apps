c0e98cdf01fce5f96633ff81a8094148ca809c0f
ReeceWoodard <rwoodar5@students.kennesaw.edu>
2026-04-22 20:29:27 -0400

    Update script.js

--- Milestone4/script.js
+++ Milestone4/script.js
@@ -1,272 +1,155 @@
-const apiKey="AIzaSyD1LDXycgXy_MazXWbDpqpYApRHg3U2g0w";
+const apiKey = "YOUR_API_KEY";
 
-let currentPage=1;
-let currentQuery="";
-let currentStart=0;
+let currentPage = 1;
+let currentQuery = "";
+let currentStart = 0;
 
+function searchBooks(query, start = 0) {
+    currentQuery = query;
+    currentStart = start;
 
-function searchBooks(query,start=0){
+    $.ajax({
+        url: "https://www.googleapis.com/books/v1/volumes",
+        data: {
+            q: query,
+            maxResults: 10,
+            startIndex: start,
+            key: apiKey
+        },
+        success: function (data) {
+            if (data.items) {
+                renderBooks(data.items, "#results");
+            } else {
+                $("#results").html("<p>No books found.</p>");
+            }
 
-currentQuery=query;
-currentStart=start;
-
-$.ajax({
-
-url:
-"https://www.googleapis.com/books/v1/volumes",
-
-data:{
-q:query,
-maxResults:10,
-startIndex:start,
-key:apiKey
-},
-
-success:function(data){
-
-renderBooks(
-data.items,
-"#results"
-);
-
-currentPage=
-Math.floor(start/10)+1;
-
-$("#pageInfo").text(
-"Page " + currentPage
-);
-
+            currentPage = Math.floor(start / 10) + 1;
+            $("#pageInfo").text("Page " + currentPage);
+        },
+        error: function () {
+            $("#results").html("<p>There was an error loading search results.</p>");
+        }
+    });
 }
 
-});
+function renderBooks(books, target) {
+    let template = $("#book-template").html();
+    let html = "";
 
+    books.forEach(function (book) {
+        let info = book.volumeInfo;
+
+        html += Mustache.render(template, {
+            id: book.id,
+            title: info.title || "No Title",
+            authors: info.authors ? info.authors.join(", ") : "Unknown Author",
+            thumbnail: info.imageLinks ? info.imageLinks.thumbnail : "https://via.placeholder.com/150"
+        });
+    });
+
+    $(target).html(html);
 }
 
+function loadDetails(bookId) {
+    $.ajax({
+        url: "https://www.googleapis.com/books/v1/volumes/" + bookId,
+        success: function (book) {
+            let info = book.volumeInfo;
+            let template = $("#detail-template").html();
 
+            let html = Mustache.render(template, {
+                title: info.title || "No Title",
+                authors: info.authors ? info.authors.join(", ") : "Unknown Author",
+                language: info.language || "N/A",
+                pages: info.pageCount || "N/A",
+                description: info.description || "No description available.",
+                thumbnail: info.imageLinks ? info.imageLinks.thumbnail : "https://via.placeholder.com/150"
+            });
 
-function renderBooks(books,target){
-
-let template=
-$("#book-template").html();
-
-let html="";
-
-books.forEach(function(book){
-
-let info=
-book.volumeInfo;
-
-html+=Mustache.render(
-template,
-{
-id:book.id,
-title:
-info.title || "No Title",
-
-authors:
-info.authors ?
-info.authors.join(", ")
-:"Unknown",
-
-thumbnail:
-info.imageLinks ?
-info.imageLinks.thumbnail
-:"https://via.placeholder.com/150"
-}
-);
-
-});
-
-$(target).html(html);
-
+            $("#details").html(html);
+        },
+        error: function () {
+            $("#details").html("<p>Could not load book details.</p>");
+        }
+    });
 }
 
-
-
-function loadDetails(bookId){
-
-$.ajax({
-
-url:
-"https://www.googleapis.com/books/v1/volumes/"
-+bookId,
-
-success:function(book){
-
-let info=
-book.volumeInfo;
-
-let template=
-$("#detail-template").html();
-
-let html=
-Mustache.render(
-template,
-{
-title:info.title,
-authors:
-info.authors ?
-info.authors.join(", ")
-:"Unknown",
-
-language:
-info.language,
-
-pages:
-info.pageCount,
-
-description:
-info.description ||
-"No description.",
-
-thumbnail:
-info.imageLinks ?
-info.imageLinks.thumbnail
-:"https://via.placeholder.com/150"
-}
-);
-
-$("#details").html(html);
-
+function loadCollection(query) {
+    $.ajax({
+        url: "https://www.googleapis.com/books/v1/volumes",
+        data: {
+            q: query,
+            maxResults: 12,
+            key: apiKey
+        },
+        success: function (data) {
+            if (data.items) {
+                renderBooks(data.items, "#collectionResults");
+            } else {
+                $("#collectionResults").html("<p>No books found in this collection.</p>");
+            }
+        },
+        error: function () {
+            $("#collectionResults").html("<p>There was an error loading the collection.</p>");
+        }
+    });
 }
 
+$("#searchBtn").click(function () {
+    let query = $("#searchInput").val().trim();
+
+    if (query !== "") {
+        searchBooks(query, 0);
+    }
 });
 
-}
-
-
-
-function loadCollection(query){
-
-$.ajax({
-
-url:
-"https://www.googleapis.com/books/v1/volumes",
-
-data:{
-q:query,
-maxResults:12,
-key:apiKey
-},
-
-success:function(data){
-
-renderBooks(
-data.items,
-"#collectionResults"
-);
-
-}
-
+$("#searchInput").keypress(function (event) {
+    if (event.which === 13) {
+        $("#searchBtn").click();
+    }
 });
 
-}
-
-
-
-$("#searchBtn").click(function(){
-
-let query=
-$("#searchInput").val();
-
-searchBooks(query);
-
+$("#nextPage").click(function () {
+    if (currentQuery !== "") {
+        searchBooks(currentQuery, currentStart + 10);
+    }
 });
 
-
-$("#nextPage").click(function(){
-
-searchBooks(
-currentQuery,
-currentStart+10
-);
-
+$("#prevPage").click(function () {
+    if (currentQuery !== "" && currentStart >= 10) {
+        searchBooks(currentQuery, currentStart - 10);
+    }
 });
 
-
-$("#prevPage").click(function(){
-
-if(currentStart>=10){
-
-searchBooks(
-currentQuery,
-currentStart-10
-);
-
-}
-
+$("#gridView").click(function () {
+    $("#results, #collectionResults")
+        .removeClass("list-view")
+        .addClass("grid-view");
 });
 
-
-$(document).on(
-"click",
-".book-card",
-function(){
-
-let id=
-$(this).data("id");
-
-loadDetails(id);
-
-}
-);
-
-
-$("#gridView").click(function(){
-
-$("#results,#collectionResults")
-.removeClass("list-view")
-.addClass("grid-view");
-
+$("#listView").click(function () {
+    $("#results, #collectionResults")
+        .removeClass("grid-view")
+        .addClass("list-view");
 });
 
-
-$("#listView").click(function(){
-
-$("#results,#collectionResults")
-.removeClass("grid-view")
-.addClass("list-view");
-
+$("#showSearch").click(function () {
+    $("#searchPanel").show();
+    $("#collectionPanel").hide();
 });
 
-
-$("#showSearch").click(function(){
-
-$("#searchPanel").show();
-$("#collectionPanel").hide();
-
+$("#showCollection").click(function () {
+    $("#searchPanel").hide();
+    $("#collectionPanel").show();
 });
 
-
-$("#showCollection").click(function(){
-
-$("#searchPanel").hide();
-$("#collectionPanel").show();
-
+$("#popularBooks").click(function () {
+    loadCollection("bestsellers");
 });
 
-
-$("#popularBooks").click(function(){
-
-loadCollection(
-"bestsellers"
-);
-
+$("#topRatedBooks").click(function () {
+    loadCollection("award winning fiction");
 });
-
-
-$("#topRatedBooks").click(function(){
-
-loadCollection(
-"award winning fiction"
-);
-
-});
-
 
 $("#collectionPanel").hide();
-
-
-loadCollection(
-"classic literature"
-);
+loadCollection("classic literature");
